4daaef0deec695f607b3d726ddbc0946ca6c4147
Matthew Petro <matthew.petro@aa.com>
2024-02-25 17:54:50 -0700

    Copy SSH host keys from secret volume to SSH directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
-#cp /mnt/keys/* /etc/ssh
-ssh-keygen -A
-chmod 600 /etc/ssh/ssh_host_*_key
-chmod 644 /etc/ssh/ssh_host_*_key.pub
+cp /mnt/keys/ssh_host_ecdsa_key /etc/ssh
+chmod 600 /etc/ssh/ssh_host_ecdsa_key
+
+cp /mnt/keys/ssh_host_ecdsa_key.pub /etc/ssh
+chmod 644 /etc/ssh/ssh_host_ecdsa_key.pub
+
+cp /mnt/keys/ssh_host_ed25519_key /etc/ssh
+chmod 600 /etc/ssh/ssh_host_ed25519_key
+
+cp /mnt/keys/ssh_host_ed25519_key.pub /etc/ssh
+chmod 644 /etc/ssh/ssh_host_ed25519_key.pub
+
+cp /mnt/keys/ssh_host_rsa_key /etc/ssh
+chmod 600 /etc/ssh/ssh_host_rsa_key
+
+cp /mnt/keys/ssh_host_rsa_key.pub /etc/ssh
+chmod 644 /etc/ssh/ssh_host_rsa_key.pub
+
 exec /usr/sbin/sshd -D -e "$@"
