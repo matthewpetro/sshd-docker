528bcfd3df6ad4cdd390628c181572827d6e4f8f
Matthew Petro <matthew.petro@aa.com>
2024-02-25 17:09:09 -0700

    Generate host keys with no comment

--- azure-create-script.sh
+++ azure-create-script.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-ssh-keygen -t ecdsa -b 256 -N "" -q -f ssh_host_ecdsa_key
-ssh-keygen -t ed25519 -N "" -q -f ssh_host_ed25519_key
-ssh-keygen -t rsa -b 2048 -N "" -q -f ssh_host_rsa_key
+ssh-keygen -t ecdsa -b 256 -N "" -q -C "" -f ssh_host_ecdsa_key
+ssh-keygen -t ed25519 -N "" -q -C "" -f ssh_host_ed25519_key
+ssh-keygen -t rsa -b 2048 -N "" -q -C "" -f ssh_host_rsa_key
 
 export SSH_HOST_ECDSA_KEY=$(cat ./ssh_host_ecdsa_key | tr -d '\n' | base64 -w 0)
 export SSH_HOST_ECDSA_KEY_PUB=$(cat ./ssh_host_ecdsa_key.pub | tr -d '\n' | base64 -w 0)
