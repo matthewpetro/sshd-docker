6c4c7cb5c58a7122dec5176c541cb6aa98970a63
Matthew Petro <matthew.petro@aa.com>
2024-02-25 17:31:53 -0700

    Don't need to remove newlines from SSH key files

--- azure-create-script.sh
+++ azure-create-script.sh
@@ -4,12 +4,12 @@ ssh-keygen -t ecdsa -b 256 -N "" -q -C "" -f ssh_host_ecdsa_key
 ssh-keygen -t ed25519 -N "" -q -C "" -f ssh_host_ed25519_key
 ssh-keygen -t rsa -b 2048 -N "" -q -C "" -f ssh_host_rsa_key
 
-export SSH_HOST_ECDSA_KEY=$(cat ./ssh_host_ecdsa_key | tr -d '\n' | base64 -w 0)
-export SSH_HOST_ECDSA_KEY_PUB=$(cat ./ssh_host_ecdsa_key.pub | tr -d '\n' | base64 -w 0)
-export SSH_HOST_ED25519_KEY=$(cat ./ssh_host_ed25519_key | tr -d '\n' | base64 -w 0)
-export SSH_HOST_ED25519_KEY_PUB=$(cat ./ssh_host_ed25519_key.pub | tr -d '\n' | base64 -w 0)
-export SSH_HOST_RSA_KEY=$(cat ./ssh_host_rsa_key | tr -d '\n' | base64 -w 0)
-export SSH_HOST_RSA_KEY_PUB=$(cat ./ssh_host_rsa_key.pub | tr -d '\n' | base64 -w 0)
+export SSH_HOST_ECDSA_KEY=$(base64 -w 0 ./ssh_host_ecdsa_key)
+export SSH_HOST_ECDSA_KEY_PUB=$(base64 -w 0 ./ssh_host_ecdsa_key.pub)
+export SSH_HOST_ED25519_KEY=$(base64 -w 0 ./ssh_host_ed25519_key)
+export SSH_HOST_ED25519_KEY_PUB=$(base64 -w 0 ./ssh_host_ed25519_key.pub)
+export SSH_HOST_RSA_KEY=$(base64 -w 0 ./ssh_host_rsa_key)
+export SSH_HOST_RSA_KEY_PUB=$(base64 -w 0 ./ssh_host_rsa_key.pub)
 
 rm ssh_host_ecdsa_key* ssh_host_ed25519_key* ssh_host_rsa_key*
 
