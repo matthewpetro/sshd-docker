449090fc4d095e4f38bc03ace904ad43233312b4
Matthew Petro <matthew.petro@aa.com>
2024-02-25 18:10:57 -0700

    Put temporary files in .tmp directory

--- azure-create-script.sh
+++ azure-create-script.sh
@@ -1,22 +1,25 @@
 #! /bin/bash
 
-ssh-keygen -t ecdsa -b 256 -N "" -q -C "" -f ssh_host_ecdsa_key
-ssh-keygen -t ed25519 -N "" -q -C "" -f ssh_host_ed25519_key
-ssh-keygen -t rsa -b 2048 -N "" -q -C "" -f ssh_host_rsa_key
+TMPDIR=.tmp
 
-export SSH_HOST_ECDSA_KEY=$(base64 -w 0 ./ssh_host_ecdsa_key)
-export SSH_HOST_ECDSA_KEY_PUB=$(base64 -w 0 ./ssh_host_ecdsa_key.pub)
-export SSH_HOST_ED25519_KEY=$(base64 -w 0 ./ssh_host_ed25519_key)
-export SSH_HOST_ED25519_KEY_PUB=$(base64 -w 0 ./ssh_host_ed25519_key.pub)
-export SSH_HOST_RSA_KEY=$(base64 -w 0 ./ssh_host_rsa_key)
-export SSH_HOST_RSA_KEY_PUB=$(base64 -w 0 ./ssh_host_rsa_key.pub)
+mkdir $TMPDIR
 
-rm ssh_host_ecdsa_key* ssh_host_ed25519_key* ssh_host_rsa_key*
+ssh-keygen -t ecdsa -b 256 -N "" -q -C "" -f $TMPDIR/ssh_host_ecdsa_key
+ssh-keygen -t ed25519 -N "" -q -C "" -f $TMPDIR/ssh_host_ed25519_key
+ssh-keygen -t rsa -b 2048 -N "" -q -C "" -f $TMPDIR/ssh_host_rsa_key
 
-envsubst < deploy-container.yaml > deploy-container.tmp.yaml
+export SSH_HOST_ECDSA_KEY=$(base64 -w 0 ${TMPDIR}/ssh_host_ecdsa_key)
+export SSH_HOST_ECDSA_KEY_PUB=$(base64 -w 0 ${TMPDIR}/ssh_host_ecdsa_key.pub)
+export SSH_HOST_ED25519_KEY=$(base64 -w 0 ${TMPDIR}/ssh_host_ed25519_key)
+export SSH_HOST_ED25519_KEY_PUB=$(base64 -w 0 ${TMPDIR}/ssh_host_ed25519_key.pub)
+export SSH_HOST_RSA_KEY=$(base64 -w 0 ${TMPDIR}/ssh_host_rsa_key)
+export SSH_HOST_RSA_KEY_PUB=$(base64 -w 0 ${TMPDIR}/ssh_host_rsa_key.pub)
+
+envsubst < deploy-container.yaml > $TMPDIR/deploy-container.tmp.yaml
 
 az container create \
   --resource-group PetroRG \
-  --file deploy-container.tmp.yaml
+  --file $TMPDIR/deploy-container.tmp.yaml
 
-rm deploy-container.tmp.yaml
+rm $TMPDIR/*
+rmdir $TMPDIR
